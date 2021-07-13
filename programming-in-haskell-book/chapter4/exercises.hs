4d39f44111630e2180547d1886497f7a500b096f
Clinton Montague <clinton@serelay.com>
2021-07-13 14:26:02 +0100

    Programming in Haskell: Chapter 4 exercises 4, 5, 6, 8 (2nd edition)

--- programming-in-haskell-book/chapter4/exercises.hs
+++ programming-in-haskell-book/chapter4/exercises.hs
@@ -1,9 +1,9 @@
--- Question 1
+-- Question 1 (first edition)
 
 halve :: [a] -> ([a], [a])
 halve xs = splitAt (length xs `div` 2) xs
 
--- Question 2
+-- Question 2 (first edition)
 safetail1 :: [a] -> [a]
 safetail1 xs = if null xs then []
                 else tail xs
@@ -17,7 +17,7 @@ safetail3 [] = []
 safetail3 (_:xs) = xs
 
 
--- Question 3
+-- Question 3 (first edition)
 or1 :: Bool -> Bool -> Bool
 or1 True True = True
 or1 True False = True
@@ -37,3 +37,30 @@ or4 :: Bool -> Bool -> Bool
 or4 True True = True
 or4 False a = a
 or4 a False = a
+
+
+-- Question 4 (first edition)
+-- True ^ True = True; _ ^ _ = False
+and' :: Bool -> Bool -> Bool
+and' a b  = if a then if b then True
+            else False else False
+
+-- Question 5 (first edition)
+-- True ^ b = b; False ^ _ = False
+and'' :: Bool -> Bool -> Bool
+and'' a b = if a then b else False
+
+-- Question 6 (first edition)
+-- mult x y z = x * y * z
+mult' :: Num a => a -> a-> a -> a
+mult' = \x -> (\y -> (\z -> x * y * z))
+
+
+-- Question 8 (second edition)
+luhnDouble :: Int -> Int
+luhnDouble n  | n < 5 = n * 2
+              | otherwise = n * 2 - 9
+
+luhn :: Int -> Int -> Int -> Int -> Bool
+luhn a b c d = (luhnDouble a + b + luhnDouble c + d) `mod` 10 == 0
+-- TODO: looking forward to being able to do that in a more Haskell way
