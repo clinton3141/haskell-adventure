f0b3afa979a41b52a761f68b018863a99c7ab707
Clinton Montague <clinton@serelay.com>
2021-07-15 18:56:35 +0100

    Programming in Haskell: C5 exercise 5

--- programming-in-haskell-book/chapter5/exercises.hs
+++ programming-in-haskell-book/chapter5/exercises.hs
@@ -13,3 +13,23 @@ square n = [(x, y) | (x, y) <- grid n n, x /= y]
 -- Question 4
 replicate :: Int -> a -> [a]
 replicate n x = [x | _ <- [1..n]]
+
+-- Question 5
+pyths :: Int -> [(Int, Int, Int)]
+pyths n = [(x, y, z) |
+            x <- [1..n],
+            y <- [x..n],
+            z <- [y..n],
+            x^2 + y^2 == z^2
+          ]
+-- Note: The question shows duplicates (3,4,5), (4,3,5)
+-- but I think that this solution is more elegant because
+-- there are no dupes. Although strictly speaking they're
+-- different triples. As asked:
+pyths' :: Int -> [(Int, Int, Int)]
+pyths' n = [(x, y, z) |
+            x <- [1..n],
+            y <- [1..n],
+            z <- [1..n],
+            x^2 + y^2 == z^2
+          ]
