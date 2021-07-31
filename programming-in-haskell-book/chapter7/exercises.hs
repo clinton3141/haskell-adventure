858ff31db09075b3d33b36a8846c1bb37049f6e2
Clinton Montague <clinton@serelay.com>
2021-07-31 18:21:10 +0100

    Programming in Haskell: C7 exercises 1-8

---
+++ programming-in-haskell-book/chapter7/exercises.hs
@@ -0,0 +1,74 @@
+-- Question 1
+q1 :: (a -> b) -> (a -> Bool) -> [a] -> [b]
+q1 f p xs = [f x | x <- xs, p x]
+
+q1' :: (a -> b) -> (a -> Bool) -> [a] -> [b]
+q1' f p xs = map f (filter p xs)
+
+-- Question 2
+all' :: (a -> Bool) -> [a] -> Bool -- Question has (a -> Bool) -> [Bool] -> Bool, but presumably this is wrong
+all' p []       = True
+all' p (x:xs)   | not (p x) = False
+                | otherwise = all' p xs
+
+any' :: (a -> Bool) -> [a] -> Bool -- Similar to above
+any' p []       = False
+any' p (x:xs)   | p x       = True
+                | otherwise = any' p xs
+
+
+takeWhile' :: (a -> Bool) -> [a] -> [a]
+takeWhile' p []     = []
+takeWhile' p (x:xs) | p x       = x : takeWhile p xs
+                    | otherwise = []
+
+dropWhile' :: (a -> Bool) -> [a] -> [a]
+dropWhile' p []     = []
+dropWhile' p (x:xs) | p x = dropWhile' p xs
+                    | otherwise = x:xs
+
+-- Question 3
+map':: (a -> b) -> [a] -> [b]
+map' f = foldr (\x xs -> (f x) : xs) []
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' p = foldr (\x xs -> if p x then x : xs else xs) []
+
+-- Question 4
+-- first attempt 🤢
+-- dec2int :: [Int] -> Int
+-- dec2int is = foldl (\r i -> r + fst i * snd i) 0 (zip (reverse is) (iterate(*10) 1))
+
+-- second 😌
+dec2int :: [Int] -> Int
+dec2int = foldl (\r i -> r * 10 + i) 0
+
+-- Question 5
+curry' :: ((a, b) -> c) -> a -> b -> c
+curry' f x y = f (x, y)
+
+uncurry' :: (a -> b -> c) -> (a, b) -> c
+uncurry' f (x, y) = f x y
+
+-- Question 6
+unfold :: (t -> Bool) -> (t -> a) -> (t -> t) -> t -> [a]
+unfold p h t x  | p x       = []
+                | otherwise = h x : unfold p h t (t x)
+
+int2bin' = unfold (==0) (`mod` 2) (`div` 2)
+
+type Bit = Int
+chop8' :: [Bit] -> [[Bit]]
+chop8' = unfold null (take 8) (drop 8)
+
+map'' :: (a -> b) -> [a] -> [b]
+map'' f = unfold null (f . head) tail
+
+-- this is very verbose, but helped me to grok it
+-- can be iterate'' = unfold (const False) id
+iterate'' :: (a -> a) -> a -> [a]
+iterate'' f x = unfold neverEmpty id f x
+            where neverEmpty = const False
+
+-- Question 7, 8
+-- See transmitter.hs
--- programming-in-haskell-book/chapter7/transmitter.hs
+++ programming-in-haskell-book/chapter7/transmitter.hs
@@ -16,9 +16,12 @@ make8 bits = take 8 (bits ++ repeat 0)
 encode :: String -> [Bit]
 encode = concat . map (make8 . int2bin . ord)
 
+chopn :: Int -> [Bit] -> [[Bit]]
+chopn _ [] = []
+chopn n bits = take n bits : chopn n (drop n bits)
+
 chop8 :: [Bit] -> [[Bit]]
-chop8 [] = []
-chop8 bits = take 8 bits : chop8 (drop 8 bits)
+chop8 = chopn 8
 
 decode :: [Bit] -> String
 decode = map (chr . bin2int) . chop8
@@ -28,3 +31,26 @@ channel = id
 
 transmit :: String -> String
 transmit = decode . channel . encode
+
+parityCheck :: [Bit] -> [Bit]
+parityCheck bs  | (even . sum) bs = bs
+                | otherwise = error "Parity check failed"
+
+
+parityBit :: [Bit] -> Bit
+parityBit = (`mod` 2) . sum
+
+encode' :: String -> [Bit]
+encode' = concat . map ((\bs -> parityBit bs : bs) . (make8 . int2bin . ord))
+
+decode' :: [Bit] -> String
+decode' = map (chr . bin2int . drop 1 . parityCheck) . chopn 9
+
+
+transmit' :: ([Bit] -> [Bit]) -> String -> String
+transmit' c = decode' . c . encode'
+
+faultyChannel = tail
+
+goodTransmitter = transmit' channel
+badTransmitter = transmit' faultyChannel
