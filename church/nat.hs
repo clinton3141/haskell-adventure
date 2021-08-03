7fa27a16032c2444a4234b18500e82f34c408f82
Clinton Montague <clinton@serelay.com>
2021-08-03 08:57:21 +0100

    Church encoding of naturals

---
+++ church/nat.hs
@@ -0,0 +1,13 @@
+data Nat = Zero | Succ Nat deriving Show
+
+nat2int :: Nat -> Int
+nat2int Zero = 0
+nat2int (Succ n) = 1 + nat2int n
+
+int2nat :: Int -> Nat
+int2nat 0 = Zero
+int2nat n = Succ (int2nat (n - 1))
+
+add :: Nat -> Nat -> Nat
+add m Zero = m
+add m (Succ n) = Succ (add m n)
